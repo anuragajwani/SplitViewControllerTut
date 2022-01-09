891e6057325c67c078f68f557b7c863fd9b582f6
Anurag Ajwani <anurag.ajwani@gmail.com>
2022-01-09 17:25:20 +0000

    Now displaying split view controller

---
+++ SplitViewControllerTut/PrimaryViewController.swift
@@ -0,0 +1,36 @@
+//
+//  PrimaryViewController.swift
+//  SplitViewControllerTut
+//
+//  Created by Anurag Ajwani on 09/01/2022.
+//
+
+import UIKit
+
+class PrimaryViewController: UIViewController {
+    
+    private let label: UILabel = {
+        let view = UILabel()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.text = "Primary"
+        return view
+    }()
+    
+    override func loadView() {
+        self.view = UIView()
+        self.view.backgroundColor = .white
+        self.setupSubviews()
+    }
+    
+    private func setupSubviews() {
+        self.setupLabel()
+    }
+    
+    private func setupLabel() {
+        self.view.addSubview(label)
+        NSLayoutConstraint.activate([
+            self.label.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+           self.label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+       ])
+    }
+}
