a4915c94dcf6771981da92c351d84b68173138c0
Anurag Ajwani <anurag.ajwani@gmail.com>
2022-01-09 17:18:27 +0000

    Initial Commit

---
+++ SplitViewControllerTut/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SplitViewControllerTut
+//
+//  Created by Anurag Ajwani on 09/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
